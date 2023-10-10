566e6a4e91e92119195f7b4bd4c11f1ce2b06be5
mhkb2216 <146079322+mhkb2216@users.noreply.github.com>
2023-10-10 14:53:31 +0500

    Update NumberGuessingGame.java

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,5 +1,38 @@
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1; // Specify the lower bound of the range
+        int upperBound = 100; // Specify the upper bound of the range
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+
+        int numberOfGuesses = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I've selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfGuesses++;
+
+            if (userGuess < lowerBound || userGuess > upperBound) {
+                System.out.println("Please guess within the specified range.");
+            } else if (userGuess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number " + randomNumber + " in " + numberOfGuesses + " guesses.");
+            }
+        }
+
+        scanner.close();
     }
 }
